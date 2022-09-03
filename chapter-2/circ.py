3ef90ee4c7d48c20d9ac74a29a26bc59bfc293fe
Torrance Khandaker <tkhandaker3@gmail.com>
2022-09-03 00:06:26 +0000

    changed all files in chapter-2 folder

---
+++ chapter-2/circ.py
@@ -0,0 +1,4 @@
+import math
+
+radius = int(input("What is the radius?: "))
+print("The area of your circle is", math.pi * (radius**2))
---
+++ chapter-2/sent.py
@@ -0,0 +1,11 @@
+var1 = "all"
+var2 = "work"
+var3 = "and"
+var4 = "no"
+var5 = "play"
+var6 = "makes"
+var7 = "jack"
+var8 = "a"
+var9 = "dull"
+var10 = "boy"
+print(var1, var2, var3, var4, var5, var6, var7, var8, var9, var10)
