6aa5a70d9b9b80d90d350396043b8f849b227d22
torrance <tkhandaker3@gmail.com>
2022-10-03 08:58:40 -0400

    fixed fizzbuzz.py

--- piglatin/fizzbuzz.py
+++ piglatin/fizzbuzz.py
@@ -1,8 +1,8 @@
 def fizzbuzz(n):
-    for number in range(int(n)):
+    for number in range(int(n) + 1):
         if number % 3 == 0 and number % 5 == 0:
             print("fizzbuzz")
-        if number % 3 == 0:
+        elif number % 3 == 0:
             print("fizz")
         elif number % 5 == 0:
             print("buzz")
