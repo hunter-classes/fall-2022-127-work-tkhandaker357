5707b9977675612c5092ee9ba2efc1acd987ad76
torrance <tkhandaker3@gmail.com>
2022-10-22 21:05:04 -0400

    added story.dat file that's read in the program to finish all extras

--- project-01-madlibs/madlibs.py
+++ project-01-madlibs/madlibs.py
@@ -1,18 +1,29 @@
+# EXTRAS COMPLETED:
+# 1: Instead of specifying the sentences or story to convert, write a story in a file and read it from your program. Make sure to include the
+#    file in your repo and that your program reads it correctly.
+# 2: Add some replacements that are unique, that is, the first time you see them you select on randomly but then you keep reusing that
+#    replacement.
+# 3: Pay attention to letter case. That is, if you replace a word at the beginning of a sentence, it should be capitalized, otherwise,
+#    lowercase.This is except in the case of proper nouns which should always be capitalized. 
+
 import random
+
+file = open("story.dat")
+story = file.read()
+story = story.split('\n')
+story = ' '.join(story)
 
 wordBank = [
     ["<VERB>", "threw", "ran", "walked", "talked", "hit", "flew", "ate", "yelled", "jumped"],
     ["<OBJECT>", "car", "hammer", "sword", "gun", "pie", "pizza", "person", "dog", "cat"],
     ["<CHAR>", "Sam", "Bob", "Alex", "Emily", "John", "Martha", "Sock", "Kevin", "Fred"],
     ["<ADVERB>", "unceremoniously", "distraughtly", "ecstatically", "furiously", "gleefully", "flamboyantly", "idiotically", "grossly",
-                "reasonably"],
-    ["<PRONOUN>", "he", "she", "they"]
+                "reasonably"]
 ]
 
-
 def replaceWords(sentence):
-    if "<HERO>" in sentence:
-        sentence = sentence.replace("<HERO>", wordBank[2][random.randrange(1, len(wordBank[2]))])
+    if "<MAINCHAR>" in sentence:
+        sentence = sentence.replace("<MAINCHAR>", wordBank[2][random.randrange(1, len(wordBank[2]))])
 
     paragraph = sentence.split('.')
     newParagraph = []
@@ -28,5 +39,4 @@ def replaceWords(sentence):
     return '.'.join(newParagraph)
 
 
-story = "<HERO> took the <OBJECT> and <VERB> it <ADVERB>. <HERO> then <VERB> an entire <OBJECT> at <CHAR>."
 print(replaceWords(story))
